3642a589afecf27700f2017d7f590c74f1572b27
Muresan Ionut <muresan.ionut95@gmail.com>
2015-10-21 14:50:35 +0300

    change status from table auto-save when status is changed

--- public/js/controllers/UserTKWJobsController.js
+++ public/js/controllers/UserTKWJobsController.js
@@ -1,13 +1,30 @@
 app.controller('UserTKWJobsController', function ($scope, $http, $routeParams) {
   $scope.jobs = [];
+  $scope.jobHashStatus = {};
 
-  $scope.status = {
-    '0': 'DRAFT',
-    '1': 'PUBLISHED',
-    '2': 'FILLED',
-    '3': 'EXPIRED',
-    '4': 'DASHBOARD'
-  };
+  $scope.status = [
+    {number: '0', text: 'DRAFT', },
+    {number: '1', text: 'PUBLISHED'},
+    {number: '2', text: 'FILLED'},
+    {number: '3', text: 'EXPIRED'},
+    {number: '4', text: 'DASHBOARD'}
+  ];
+
+  $scope.save = function(id) {
+    var job = {};
+    for(var i=0; i<$scope.jobs.length; i++){
+      if( $scope.jobs[i].id == id){
+        job = $scope.jobs[i];
+      }
+    }
+    $http.put('/api/jobs/' + job.id + generateUrlKey(), {job: {description: job.description, status: $scope.jobHashStatus[job.id].number}}).
+    success(function(data, status, headers, config) {
+    }).
+    error(function(data, status, headers, config) {
+      logged(data)
+    });
+    getJobs();
+  }
 
   $scope.deleteJob = function(id){
     $http.delete('/api/jobs/' + id + generateUrlKey()).
@@ -19,11 +36,18 @@ app.controller('UserTKWJobsController', function ($scope, $http, $routeParams) {
     });
   };
 
+  completeStatus = function(){
+    for(var i = 0; i < $scope.numberOfJobs; i++){
+      $scope.jobHashStatus[$scope.jobs[i].id] = $scope.status[$scope.jobs[i].status]
+    }
+  }
+
   getJobs = function(){
     $http.get('api/jobs' + generateUrlKey()).
     success(function(data){
       $scope.jobs = data['body'];
       $scope.numberOfJobs = $scope.jobs.length;
+      completeStatus();
     }).
     error(function(data) {
       logged(data)
